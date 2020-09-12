07b6539c1ad4c90461685c1ebabbda377b456a32
Trammell hudson <hudson@trmm.net>
2020-09-12 03:50:35 -0400

    micropython websocket version

---
+++ ubustime.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from uwebsockets import client
+import gvbparser
+import logging
+logging.basicConfig(filename='ubustime.log',level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
+
+
+# haarlemmerplein and # ponts Buiksloterweg, Distelweg, NDSM
+halts = [ "02133", "02114", "09906", "09901", "09902" ]
+
+ws = client.WebsocketClient("wss://maps-wss.gvb.nl/")
+msg = ws.recv()
+print(msg)
+
+print("connected: adding stops")
+for halt in halts:
+	ws.send('[5,"/stops/%s"]' % (halt))
+#ws.send(b'[5,"/stops/02133"]\r\n')
+
+
+line = ''
+while ws.open:
+	# we want to receive a line at a time
+	msg = ws.recv()
+	if msg is None:
+		continue
+
+	gvbparser.parse(msg)
+
+print("EXIT")
