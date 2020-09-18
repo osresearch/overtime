b571d2c17ef5cf9c65a5c5d2e219c32e14f9e4f0
Trammell Hudson <hudson@trmm.net>
2020-09-18 17:57:44 -0400

    document config

--- overtime/config.h
+++ overtime/config.h
@@ -1,10 +1,24 @@
+#ifndef _overtime_config_h_
+#define _overtime_config_h_
 
-#define WIFI_SSID "xyzzy"
+/*
+ * Add your wifi credentials so the ESP can get on your network.
+ */
+#define WIFI_SSID "NETWORKNAME"
 #define WIFI_PASSWORD "PASSWORD"
 
-// "[5,\"/stops/02113\"]", // haarlemmerplein
-// "[5,\"/stops/02114\"]", // haarlemmerplein
-// "[5,\"/stops/09906\"]", // buiksloterweg
-// "[5,\"/stops/09901\"]", // distelweg
-// "[5,\"/stops/09902\"]", // ndsm
+/*
+ * Add the stops that you want to track.
+ * These can be found on the maps.gvb.nl website by clicking
+ * on the stop.  Note that the leading 0 is important, so they
+ * are strings, not numbers.
+ */
 #define GVB_HALTS "02113", "02114", "09906", "09901", "09902"
+
+/*
+ * Ignore buses/trams/ferries that are too close to reach in time.
+ * This only shows trips that are more than five minutes away.
+ */
+#define ARRIVAL_THRESHOLD 300
+
+#endif
